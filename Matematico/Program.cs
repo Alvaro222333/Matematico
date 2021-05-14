0bb05fa9e5c6fa67cc55410ef0de7cdc30490f5e
alvaro222333 <alvaro222333@gmail.com>
2021-05-13 22:36:41 -0400

    Agregar archivos de proyecto.

---
+++ Matematico/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matematico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num1, Num2, Suma;
+            Console.Write("Introduzca primer numero: ") ;
+            Num1 = int.Parse(Console.ReadLine());
+            Console.Write("Introduzca segundo numero: ");
+            Num2 = int.Parse(Console.ReadLine());
+
+            Suma = Num1 + Num2;
+
+            Console.WriteLine("La suma tota es {0}", Suma);
+
+            Console.ReadKey();
+        }
+    }
+}
