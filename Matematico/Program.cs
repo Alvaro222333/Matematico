f3088064e1253fb33a4bf34442105c347b1b227b
alvaro222333 <alvaro222333@gmail.com>
2021-05-14 00:03:15 -0400

    se agrego una cogido para multiplicar

--- Matematico/Program.cs
+++ Matematico/Program.cs
@@ -10,7 +10,7 @@ namespace Matematico
     {
         static void Main(string[] args)
         {
-            int Num1, Num2, Suma, Resta;
+            int Num1, Num2, Suma, Resta, Multiplicacion;
             Console.Write("Introduzca primer numero: ") ;
             Num1 = int.Parse(Console.ReadLine());
             Console.Write("Introduzca segundo numero: ");
@@ -18,9 +18,10 @@ namespace Matematico
 
             Suma = Num1 + Num2;
             Resta = Num1 + Num2;
-
+            Multiplicacion = Num1 + Num2;
             Console.WriteLine("La suma total es {0}", Suma);
             Console.WriteLine("el total de la resta es {0}", Resta);
+            Console.WriteLine("el total de la multiplicacion es {0}", Multiplicacion);
 
             Console.Write("Se agrego una 1ra linea");
             
